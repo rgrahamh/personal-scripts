f42dff0f5f7a6ec1e885d40ae2f075b10b3a1558
Ryan Houck <ryan_houck@taylor.edu>
2019-01-21 20:53:06 -0500

    Added set-up-cron-jobs.sh (I built the script for Corpus Christi (the open source project), and it was too useful to pass up saving).

---
+++ set-up-cron-jobs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+crontab -l > tempcron
+
+#Enter all new jobs to be created as a new entry in the array:
+JOBS[0]='30 3 * * * $(cd $(pwd); . ./set-up-bash.sh; flask maintain prune-events) >/dev/null 2>&1'
+JOBS[1]='0 0 * * 0 /usr/local/bin/bupServ.sh >/dev/null 2>&1'
+NUM_JOBS=2
+
+#Making sure that each job isn't already in the crontab
+i=0
+while [ $i -lt $NUM_JOBS ]; do
+    isInserted=false
+    while read job; do
+        if [[ ${JOBS[i]} == $job ]]; then
+            echo "Ignoring the job <${JOBS[i]}> because it's already in the crontab"
+            isInserted=true
+            break
+        fi
+    done < tempcron
+    if [ $isInserted = false ]; then
+        echo "${JOBS[i]}" >> tempcron
+        echo "Added job <${JOBS[i]}>."
+    fi
+    i=$((i + 1))
+done
+
+#Enter tempcron into the crontab
+crontab tempcron
+
+#Interpret exit code
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    echo "Successfully entered the crontab!"
+else
+    echo "Crontab failed with error code $RESULT."
+fi
+
+#cleanup
+rm tempcron
+exit $RESULT
