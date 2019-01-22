0d212b649684f9cb3f1785d4236194754b3a5a3d
Ryan Houck <ryan_houck@taylor.edu>
2019-01-21 22:22:09 -0500

    Altered set-up-cron-jobs.sh so that it tries to insert individual jobs into the crontab rather than fail if just one job is wrong, changed the print statments slightly.

--- set-up-cron-jobs.sh
+++ set-up-cron-jobs.sh
@@ -2,9 +2,9 @@
 
 crontab -l > tempcron
 
-#Enter all new jobs to be created as a new entry in the array:
-JOBS[0]='30 3 * * * $(cd $(pwd); . ./set-up-bash.sh; flask maintain prune-events) >/dev/null 2>&1'
-JOBS[1]='0 0 * * 0 /usr/local/bin/bupServ.sh >/dev/null 2>&1'
+#Enter all new jobs to be created as a new entry in the array (JOBS[1], JOBS[2], etc).
+JOBS[0]='30 3 * * * /usr/local/bin/bupServ >/dev/null 2>&1'
+
 NUM_JOBS=${#JOBS[@]}
 
 #Making sure that each job isn't already in the crontab
@@ -12,29 +12,34 @@ i=0
 while [ $i -lt $NUM_JOBS ]; do
     isInserted=false
     while read job; do
-        if [[ ${JOBS[i]} == $job ]]; then
-            echo "Ignoring the job <${JOBS[i]}> because it's already in the crontab"
+        if [[ "${JOBS[i]}" == "$job" ]]; then
+            echo "[~] Ignoring the job <${JOBS[i]}> because it's already in the crontab."
             isInserted=true
             break
         fi
     done < tempcron
     if [ $isInserted = false ]; then
+        #Put job into the tempcron
         echo "${JOBS[i]}" >> tempcron
-        echo "Added job <${JOBS[i]}>."
+
+        #Enter job into the crontab
+        crontab tempcron >/dev/null 2>&1
+
+        #Interpret exit code
+        RESULT=$?
+        if [ $RESULT -eq 0 ]; then
+            echo "[O] Successfully entered <${JOBS[i]}>!"
+        else
+            echo "[X] Crontab failed with error code $RESULT while trying to enter <${JOBS[i]}>."
+
+            #Refresh the tempcron
+            crontab -l > tempcron
+        fi
     fi
     i=$((i + 1))
 done
 
-#Enter tempcron into the crontab
-crontab tempcron
 
-#Interpret exit code
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-    echo "Successfully entered the crontab!"
-else
-    echo "Crontab failed with error code $RESULT."
-fi
 
 #cleanup
 rm tempcron
